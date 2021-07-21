ee3cf018b9393baa07599ac99668315b27baf6e9
Prometheus <berjistechnologies@gmail.com>
2021-07-21 16:44:11 +0300

    laptop user verifiable columns, laptop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
     dashboard_path()
   end
 
+  def after_sign_up_path_for(resource)
+    account_path(current_user.id)
+  end
+
   def after_sign_out_path_for(resource_or_scope)
-    request.referrer
+    request.referrer || root_path
   end
 end
--- db/migrate/20210717205741_add_confirmable_to_devise.rb
+++ db/migrate/20210717205741_add_confirmable_to_devise.rb
@@ -16,8 +16,8 @@ class AddConfirmableToDevise < ActiveRecord::Migration[6.1]
   end
 
   def down
-    remove_index :users, :confirmation_token
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    remove_columns :users, :unconfirmed_email # Only if using reconfirmable
+    # remove_index :users, :confirmation_token
+    # remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    # remove_columns :users, :unconfirmed_email # Only if using reconfirmable
   end
 end
