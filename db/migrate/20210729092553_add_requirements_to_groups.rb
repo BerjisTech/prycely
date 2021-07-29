f2c72c6b082f682dd40eb13a2ec51cefb7880df7
Prometheus <berjistechnologies@gmail.com>
2021-07-29 12:26:21 +0300

    Add requirements to groups

---
+++ db/migrate/20210729092553_add_requirements_to_groups.rb
@@ -0,0 +1,5 @@
+class AddRequirementsToGroups < ActiveRecord::Migration[6.1]
+  def change
+    add_column :groups, :requirements, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_181900) do
+ActiveRecord::Schema.define(version: 2021_07_29_092553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,7 @@ ActiveRecord::Schema.define(version: 2021_07_25_181900) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "description"
+    t.text "requirements"
   end
 
   create_table "grouptypes", force: :cascade do |t|
