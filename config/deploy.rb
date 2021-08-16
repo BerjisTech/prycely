ca1fa011f205995128d43842108f798978a0f628
Prometheus <berjistechnologies@gmail.com>
2021-08-16 23:57:43 +0300

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, "git@github.com:BerjisTech/prycely.git"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, "/home/prycely/#{fetch :application}"
+set :deploy_to, "/home/prometheus/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
