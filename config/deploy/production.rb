f9afedbcc4dfe069a6e806560c3b707c3efe11cb
Prometheus <berjistechnologies@gmail.com>
2021-08-16 23:52:27 +0300

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'prycely.com', user: 'prycely', roles: %w{app db web}
+server 'prycely.com', user: 'prometheus', roles: %w{app db web}
 
 
 # role-based syntax
