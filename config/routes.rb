ddc750004e30a7cd5c02e8cd2608e7426c792191
Prometheus <berjistechnologies@gmail.com>
2021-08-17 02:07:23 +0300

    devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -47,8 +47,7 @@ Rails.application.routes.draw do
   resources :groups
 
   resources :wallets
-  devise_for :users, controllers: { confirmations: "confirmations" }, path: "",
-                     path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
+  devise_for :users, controllers: { confirmations: "confirmations" }, path: "", path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
 
   root "home#index"
 
