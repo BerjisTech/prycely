ecab36a0b7b001e7479e0931024e69a2d521e470
Prometheus <berjistechnologies@gmail.com>
2021-08-19 15:17:56 +0300

    Dashboard wallet cards

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -6,4 +6,12 @@ class Wallet < ApplicationRecord
   def self.mine(user_id)
     Wallet.where(user_id: user_id)
   end
+
+  def self.dashboard_colors
+    colors = []
+    colors << ["FDEDEE","E14141"]
+    colors << ["D8F6F0","52DFB4"]
+    colors << ["D0E7FF","0147F3"]
+    colors << ["F9F5E6","F2BD42"]
+  end
 end
