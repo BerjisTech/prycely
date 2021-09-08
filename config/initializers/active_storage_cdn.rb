b7c26fb51a44a07feacc486d65e341b93d532ed1
Prometheus <berjistechnologies@gmail.com>
2021-09-08 13:08:43 +0300

    Active storage error, add uuid to users table

--- config/initializers/active_storage_cdn.rb
+++ config/initializers/active_storage_cdn.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.config.after_initialize do
-    require "active_storage/service/s3_service"
-  
-    module SimpleCDNUrlReplacement
-      CDN_HOST = "assets.prycely.com"
-  
-      def url(...)
-        url = super
-        original_host = "#{bucket.name}.#{client.client.config.endpoint.host}"      
-        url.gsub(original_host, CDN_HOST)
-      end
+  require 'active_storage/service/s3_service'
+
+  module SimpleCDNUrlReplacement
+    CDN_HOST = 'assets.prycely.com'
+
+    def url(...)
+      url = super
+      original_host = "#{bucket.name}.#{client.client.config.endpoint.host}"
+      url.gsub(original_host, CDN_HOST)
     end
-  
-    ActiveStorage::Service::S3Service.prepend(SimpleCDNUrlReplacement)
   end
+
+  ActiveStorage::Service::S3Service.prepend(SimpleCDNUrlReplacement)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_905_183_049) do
+ActiveRecord::Schema.define(version: 20_210_908_090_254) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
 
   create_table 'accounts', force: :cascade do |t|
@@ -342,7 +343,8 @@ ActiveRecord::Schema.define(version: 20_210_905_183_049) do
     t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
+  create_table 'users', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.bigint 'integer_id'
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
