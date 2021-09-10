a3955a3e32e5056dad8014598d84015d6bdbf947
Prometheus <berjistechnologies@gmail.com>
2021-09-10 10:03:54 +0300

    Commented out active_storage_cdn.rb to stop the redirect of amazon s3 to digitalocean spaces

--- config/initializers/active_storage_cdn.rb
+++ config/initializers/active_storage_cdn.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-Rails.application.config.after_initialize do
-  require 'active_storage/service/s3_service'
+# Rails.application.config.after_initialize do
+#   require 'active_storage/service/s3_service'
 
-  module SimpleCDNUrlReplacement
-    CDN_HOST = 'assets.prycely.com'
+#   module SimpleCDNUrlReplacement
+#     CDN_HOST = 'prycely.s3.us-east-2.amazonaws.com/'
 
-    def url(...)
-      url = super
-      original_host = "#{bucket.name}.#{client.client.config.endpoint.host}"
-      url.gsub(original_host, CDN_HOST)
-    end
-  end
+#     def url(...)
+#       url = super
+#       original_host = "#{bucket.name}.#{client.client.config.endpoint.host}"
+#       url.gsub(original_host, CDN_HOST)
+#     end
+#   end
 
-  ActiveStorage::Service::S3Service.prepend(SimpleCDNUrlReplacement)
-end
+#   ActiveStorage::Service::S3Service.prepend(SimpleCDNUrlReplacement)
+# end
