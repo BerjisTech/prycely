08c3aa5c13f29758606aa0056a2a758f4cad8b44
Prometheus <berjistechnologies@gmail.com>
2021-09-11 16:27:35 +0300

    Use response directly on process_paybill_response

--- app/models/paybill.rb
+++ app/models/paybill.rb
@@ -3,40 +3,40 @@
 class Paybill < ApplicationRecord
   def self.process_paybill_response(response)
 
-    paybill_details = {
-      transactionType: response['TransactionType'],
-      mpesaCode: response['TransID'],
-      payBillBalance: response['OrgAccountBalance'],
-      thirdPartyTransID: response['ThirdPartyTransID'],
-      invoiceNumber: response['InvoiceNumber'],
-      amount: response['TransAmount'],
-      firstName: response['FirstName'] || '',
-      lastName: response['LastName'] || '',
-      middleName: response['MiddleName'] || '',
-      phone: response['MSISDN'],
-      shortCode: response['BusinessShortCode'],
-      accountNumber: response['BillRefNumber'].upcase
-    }
+    # paybill_details = {
+    #   transactionType: response['TransactionType'],
+    #   mpesaCode: response['TransID'],
+    #   payBillBalance: response['OrgAccountBalance'],
+    #   thirdPartyTransID: response['ThirdPartyTransID'],
+    #   invoiceNumber: response['InvoiceNumber'],
+    #   amount: response['InvoiceNumber'],
+    #   firstName: response['FirstName'] || '',
+    #   lastName: response['LastName'] || '',
+    #   middleName: response['MiddleName'] || '',
+    #   phone: response['MSISDN'],
+    #   shortCode: response['BusinessShortCode'],
+    #   accountNumber: response['BillRefNumber'].upcase
+    # }
 
-    create_deposit(paybill_details, response)
-    create_or_update_transaction(paybill_details, response)
+    create_deposit(response)
+    create_or_update_transaction(response)
   end
 
-  def self.create_deposit(paybill_details, response)
+  def self.create_deposit(response)
     paybill_data = Paybill.new(
       'request': response,
-      'paybill_type': paybill_details.transactionType,
-      'transaction_reference': paybill_details.mpesaCode,
-      'paybill_balance': paybill_details.payBillBalance,
-      'third_party_transaction_id': paybill_details.thirdPartyTransID,
-      'invoice_number': paybill_details.invoiceNumber,
-      'amount': paybill_details.amount,
-      'first_name': paybill_details.firstName,
-      'last_name': paybill_details.lastName,
-      'middle_name': paybill_details.middleName,
-      'phone': paybill_details.phone,
-      'short_code': paybill_details.shortCode,
-      'account_number': paybill_details.accountNumber
+      'paybill_type': response['TransactionType'],
+      'transaction_reference': response['TransID'],
+      'paybill_balance': response['OrgAccountBalance'],
+      'third_party_transaction_id': response['ThirdPartyTransID'],
+      'invoice_number': response['InvoiceNumber'],
+      'amount': response['TransAmount'],
+      'first_name': response['FirstName'],
+      'last_name': response['LastName'],
+      'middle_name': response['MiddleName'],
+      'phone': response['MSISDN'],
+      'short_code': response['BusinessShortCode'],
+      'account_number': response['BillRefNumber'].upcase
     )
     begin
       paybill_data.save
@@ -45,10 +45,10 @@ class Paybill < ApplicationRecord
     end
   end
 
-  def self.create_or_update_transaction(paybill_details, _response)
-    check_trans = Transaction.find_by(transaction_reference: 'refererefe')
+  def self.create_or_update_transaction(response)
+    check_trans = Transaction.find_by(transaction_reference: response['TransID'])
 
-    Transaction.update_paybill_tansaction(paybill_details.mpesaCode, paybill_details.amount) if check_trans.present?
+    Transaction.update_paybill_tansaction(response['TransID'], response['TransAmount']) if check_trans.present?
   end
 
   def self.update_deposit; end
