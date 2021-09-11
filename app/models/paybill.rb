602f175e9aace771176f140d81a3af7701f665cc
Prometheus <berjistechnologies@gmail.com>
2021-09-11 15:57:29 +0300

    replace strtoupper with .capitalize

--- app/models/paybill.rb
+++ app/models/paybill.rb
@@ -15,7 +15,7 @@ class Paybill < ApplicationRecord
       middleName: response['MiddleName'] || '',
       phone: response['MSISDN'],
       shortCode: response['BusinessShortCode'],
-      accountNumber: strtoupper(response['BillRefNumber'])
+      accountNumber: response['BillRefNumber'].upcase
     }
 
     create_deposit(paybill_details, response)
