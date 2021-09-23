92284e5a7544f6e1a0209ac106c4f5d4c12d1313
Prometheus <berjistechnologies@gmail.com>
2021-09-23 20:18:59 +0300

    Remove group and wallet relation from transaction.rb

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
-  belongs_to :group
-  belongs_to :wallet
+  # belongs_to :group
+  # belongs_to :wallet
 
   def self.for_group(group_id, limit = 0, offset = 0)
     Transaction.where(group_id: group_id).limit(limit).offset(offset)
