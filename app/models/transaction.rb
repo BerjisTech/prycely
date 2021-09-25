8e387ce4b8d383a933a756c40386070eb3d7edce
Prometheus <berjistechnologies@gmail.com>
2021-09-25 22:12:52 +0300

    Check status in Transaction.for_group and Transaction.total

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,11 +5,11 @@ class Transaction < ApplicationRecord
   # belongs_to :wallet
 
   def self.for_group(group_id, limit = 0, offset = 0)
-    Transaction.where(group_id: group_id).limit(limit).offset(offset)
+    Transaction.where(group_id: group_id, status: 1).limit(limit).offset(offset)
   end
 
   def self.total(group_id)
-    Transaction.where(group_id: group_id).pluck('count(id)').first
+    Transaction.where(group_id: group_id, status: 1).pluck('count(id)').first
   end
 
   def self.create_from_stk(amount, response, user, description, level, account, currency)
