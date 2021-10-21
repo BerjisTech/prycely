e5912b16e44a15bcd1729609964a4bac748bf20c
Prometheus <berjistechnologies@gmail.com>
2021-10-21 22:51:44 +0300

    bundler:2.2.28, add user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
     end_date = (start_date - range)
     Transaction.where(user_id: user_id, transaction_type: 2, status: 1).where('created_at <? ', start_date).where(
       'created_at >?', end_date
-    ).pluck(:sum(amount), :currency).first.to_f
+    ).pluck('sum(amount)').first.to_f
   end
 
   def self.range_savings(user_id, start_date, range)
