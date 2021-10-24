2b0d676f4757b65fdcbc8abf3b4caf53bb6d02f6
Prometheus <berjistechnologies@gmail.com>
2021-10-25 01:48:55 +0300

    new group form on all groups page

---
+++ db/migrate/20211024212402_change_group_type_to_uuid.rb
@@ -0,0 +1,5 @@
+class ChangeGroupTypeToUuid < ActiveRecord::Migration[6.1]
+  def change
+    execute "ALTER TABLE groups ALTER COLUMN group_type SET DATA TYPE UUID USING (uuid(lpad(replace(text(group_type),'-',''), 32, '0')));"
+  end
+end
