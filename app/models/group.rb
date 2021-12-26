c3cde0aff3a15965e744acc710c3ba18fdb2ccc0
Prometheus <berjistechnologies@gmail.com>
2021-12-26 18:07:05 +0300

    Group transactions graph

--- app/models/group.rb
+++ app/models/group.rb
@@ -70,4 +70,11 @@ class Group < ApplicationRecord
   def self.user_savings(user_id, group_id)
     user_credit(group_id, user_id) - user_debit(group_id, user_id)
   end
+
+  def self.transactions(group_id, date_start, date_end)
+    Transaction.where(group_id: group_id, level: 1, status: 1)
+                .where(created_at: date_start..date_end)
+                .select('sum(CASE WHEN transaction_type = 1 THEN amount ELSE 0 END) as credit, sum(CASE WHEN transaction_type = 2 THEN amount ELSE 0 END) as debit, currency, DATE(created_at) as date')
+                .group('date, currency')
+  end
 end
