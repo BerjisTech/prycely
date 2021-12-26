b38b48f2191d35a635349446c1e224c50ae993e9
Prometheus <berjistechnologies@gmail.com>
2021-12-26 17:18:01 +0300

    Ensure account name returns value even if account is nil

--- app/models/account.rb
+++ app/models/account.rb
@@ -35,9 +35,10 @@ class Account < ApplicationRecord
 
   def self.full_names(user_id)
     account = Account.find_by(user_id: user_id)
-    if account.nil?
-      account = {first_name: "--", last_name: "--"}
+    if account.present?
+      "#{account.first_name.humanize} #{account.last_name.humanize}"
+    else
+      "-- --"
     end
-    "#{account.first_name.humanize} #{account.last_name.humanize}"
   end
 end
