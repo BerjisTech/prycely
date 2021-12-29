fa666d2307a6b6d9a0f22eff83e4aca7ab011d98
Prometheus <berjistechnologies@gmail.com>
2021-12-29 15:08:43 +0300

    Transaction .order(date: :asc)

--- app/models/group.rb
+++ app/models/group.rb
@@ -100,7 +100,7 @@ class Group < ApplicationRecord
     date_end = Date.today - to.to_i.days
     Transaction.where(group_id: group_id, level: 1, status: 1)
                .where(created_at: date_start..date_end)
-               .order(date: :asc)
+               .order(created_at: :asc)
                .select('*')
   end
 end
