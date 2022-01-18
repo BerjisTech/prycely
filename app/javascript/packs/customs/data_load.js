66a0bfd5535901b12f134da70d5d43e4bf9515d7
Prometheus <berjistechnologies@gmail.com>
2022-01-18 12:25:09 +0300

    Data load functions fixed

--- app/javascript/packs/customs/data_load.js
+++ app/javascript/packs/customs/data_load.js
@@ -1,7 +1,6 @@
 $(document).ready(() => {
 
     let fetch_data = (from, to) => {
-        $('.group_projects').html('<img src="http://assets.prycely.com/images/preloader.gif" style="width: 100%; height: auto;">')
         $.ajax({
             url: `${base_url}fetch_group_${page_title}`,
             method: 'POST',
@@ -11,18 +10,19 @@ $(document).ready(() => {
                 'to': to
             },
             success: (response) => {
-                $('.group_projects').html(response)
+                $('.fetched_data_js_block').html(response)
             },
             error: (response) => {
-                $('.group_projects').html(`<div style="width: 100%; height: 100%;" class="m-3 p-3 d-flex align-items-center justify-content-center">There has been an error fetching your ${page_title}</div>`)
+                $('.fetched_data_js_block').html(`<div style="width: 100%; height: 100%;" class="m-3 p-3 d-flex align-items-center justify-content-center">There has been an error fetching your ${page_title}</div>`)
             }
         })
     }
 
-    $('.fetch_projects').on('change', (e) => {
+    $('.fetched_data_js_select').on('change', (e) => {
+        $('.fetched_data_js_block').html('<img src="http://assets.prycely.com/images/preloader.gif" style="width: 100%; height: auto;">')
         fetch_data($(e.target).val(), 0)
     })
 
     if (window.location.href.includes(`/${page_title}/g/`))
-        fetch_data(300, 200)
+        fetch_data(7, 0)
 })
