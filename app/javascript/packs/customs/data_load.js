c8a1425e94134863092ec74764fd96dc8bc0a98b
Prometheus <berjistechnologies@gmail.com>
2022-01-28 12:45:48 +0300

    add more plain js to data_load

--- app/javascript/packs/customs/data_load.js
+++ app/javascript/packs/customs/data_load.js
@@ -1,5 +1,4 @@
-$(document).ready(() => {
-
+document.addEventListener("DOMContentLoaded", function (event) {
     let fetch_data = (from, to) => {
         $.ajax({
             url: `${base_url}fetch_group_${page_title.replace('group_', '')}`,
@@ -10,10 +9,10 @@ $(document).ready(() => {
                 'to': to
             },
             success: (response) => {
-                $('.fetched_data_js_block').html(response)
+                document.querySelector('.fetched_data_js_block').innerHTML = response
             },
             error: (response) => {
-                $('.fetched_data_js_block').html(`<div style="width: 100%; height: 100%;" class="m-3 p-3 d-flex align-items-center justify-content-center">There has been an error fetching your ${page_title}</div>`)
+                document.querySelector('.fetched_data_js_block').innerHTML = `<div style="width: 100%; height: 100%;" class="m-3 p-3 d-flex align-items-center justify-content-center">There has been an error fetching your ${page_title}</div>`
             }
         })
     }
@@ -25,4 +24,4 @@ $(document).ready(() => {
 
     if (window.location.href.includes(`/${page_title}/g/`))
         fetch_data(7, 0)
-})
+});
