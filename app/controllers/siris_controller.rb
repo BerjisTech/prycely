1e0b78033aa72af3ff8de0976faa41f9d3db1be0
prometheus <berjistechnologies@gmail.com>
2022-05-05 01:47:30 +0300

    add _ to siri methods

--- app/controllers/siris_controller.rb
+++ app/controllers/siris_controller.rb
@@ -5,23 +5,23 @@ class SirisController < ApplicationController
   before_action :set_siri, only: %i[show edit update destroy]
 
   # GET /siris or /siris.json
-  def index
+  def _index
     @siris = Siri.all
   end
 
   # GET /siris/1 or /siris/1.json
-  def show; end
+  def _show; end
 
   # GET /siris/new
-  def new
+  def _new
     @siri = Siri.new
   end
 
   # GET /siris/1/edit
-  def edit; end
+  def _edit; end
 
   # POST /siris or /siris.json
-  def create
+  def _create
     @siri = Siri.new(siri_params)
 
     respond_to do |format|
@@ -36,7 +36,7 @@ class SirisController < ApplicationController
   end
 
   # PATCH/PUT /siris/1 or /siris/1.json
-  def update
+  def _update
     respond_to do |format|
       if @siri.update(siri_params)
         format.html { redirect_to @siri, notice: 'Siri was successfully updated.' }
@@ -49,7 +49,7 @@ class SirisController < ApplicationController
   end
 
   # DELETE /siris/1 or /siris/1.json
-  def destroy
+  def _destroy
     @siri.destroy
     respond_to do |format|
       format.html { redirect_to siris_url, notice: 'Siri was successfully destroyed.' }
